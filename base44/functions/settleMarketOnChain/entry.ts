0f5134a50d2df587b235f07bb4c7dcd0cd6207f4
Jorge Cortez <atvrentalssantateresa@gmail.com>
2026-06-03 03:54:45 +0000

    File changes

--- base44/functions/settleMarketOnChain/entry.ts
+++ base44/functions/settleMarketOnChain/entry.ts
@@ -43,7 +43,7 @@ Deno.serve(async (req) => {
     );
 
     const [platformPda] = PublicKey.findProgramAddressSync(
-      [Buffer.from('platform_config')],
+      [Buffer.from('platform')],
       programId
     );
 
